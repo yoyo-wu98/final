50f6fa26968e7b02f73ab3f011c05055c8810e02
JyhWind <493552898@qq.com>
2019-12-18 18:40:54 +0800

    2019-12-18-3

--- __init__.py
+++ __init__.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-
 from flask import Flask
-from . import Auth
-from .users import *
 
 
-if __name__== "__main__":
+if __name__=="__main__":
     app = Flask(__name__)
-    app.register_blueprint(Auth.bp)
+    app.register_blueprint()
     app.config['JSON_AS_ASCII'] = False
     app.run(debug=True)
